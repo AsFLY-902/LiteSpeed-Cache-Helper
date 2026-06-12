0fa9ed32a0ec4a538212697773585076b265428e
Ashok Patel <37661597+AsFLY-902@users.noreply.github.com>
2026-06-12 13:40:38 +0530

    Add self_nonce parameter to optimization functions

--- src/const.php
+++ src/const.php
@@ -10,7 +10,7 @@
     'clear_err_domains' => [ 'function' => 'clear_err_domains', 'message_ok' => 'Error Domains cleared', 'message_error' => 'Error Domains NOT cleared' ],
     'clear_disabled_nodes' => [ 'function' => 'clear_disabled_nodes', 'message_ok' => 'Disabled nodes cleared', 'message_error' => 'Disabled nodes NOT cleared' ],
     'clear_settings' => [ 'function' => 'clear_settings', 'message_ok' => 'All settings cleared', 'message_error' => 'Settings NOT cleared' ],
-    'redetect_image_node' => [ 'function' => 'redetect_image_node', 'message_ok' => 'Image optimization node updated', 'message_error' => 'Failed to update image optimization node' ],
-    'redetect_page_node' => [ 'function' => 'redetect_page_node', 'message_ok' => 'Page optimization node updated', 'message_error' => 'Failed to update page optimization node' ],
-    'reset_ttl' => [ 'function' => 'reset_ttl', 'message_ok' => 'Page optimization services TTL reset', 'message_error' => 'Failed to reset TTL' ],                                                                                         
+    'redetect_image_node' => [ 'function' => 'redetect_image_node', 'self_nonce' => true, 'message_ok' => 'Image optimization node updated', 'message_error' => 'Failed to update image optimization node' ],
+    'redetect_page_node' => [ 'function' => 'redetect_page_node', 'self_nonce' => true, 'message_ok' => 'Page optimization node updated', 'message_error' => 'Failed to update page optimization node' ],
+    'reset_ttl' => [ 'function' => 'reset_ttl', 'self_nonce' => true, 'message_ok' => 'Page optimization services TTL reset', 'message_error' => 'Failed to reset TTL' ],                                                                                         
 ]);
